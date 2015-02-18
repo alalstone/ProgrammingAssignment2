1a4f609f996aa26f4b6a09218bc8a622225be719
Andrea Alstone <alalstone@lbl.gov>
2015-02-17 22:30:42 -0800

    more thorough comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+# cachematrix.R contains functions that make use of the cache and take advantage
+# of scoping rules in R. makeCacheMatrix creates a list object with functions to 
+# get and set the the values and inverse of a square invertable matrix. cacheSolve 
+# acts on a list created using makeCacheMatrix and uses the solve() fucntion to 
+# calculate the matrix inverse and store the value in the cache, if the 
+# inverse already exists in the cache it is retrived saving computation time
 
-## makeCacheMatrix creates a special matrix of x that caches it's inverse
+#  makeCacheMatrix return a list for input matrix x with functions to:
+#  1.set the value of the matrix
+#  2.get the value of the matrix
+#  3.set the inverse of the matrix
+#  4.get the inverse of the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -18,9 +27,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve looks for the inverse of matrix x in cached data, if the inverse
-## exists it returns it, if not it calculates the inverse, which is then cached
-## and returned
+## cacheSolve looks for the inverse of a 'matrix' created using makeCacheMatrix 
+## in cached data, if the inverse exists it returns it, if not it calculates 
+## the inverse, which is then cached and returned
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
